0b660dd56d1641cfab053129e00b20357442c70f
Kimmo Kotajärvi <kimmo.kotajarvi@gmail.com>
2024-01-26 17:16:41 -0800

    Kludge together a financial report with a spend/gain graph

--- mygame/app/strategy_evaluation.rb
+++ mygame/app/strategy_evaluation.rb
@@ -9,7 +9,9 @@ module Strategy
       money_spent:       0,
       money_gained:      0,
       reputation_gained: 0,
-      reputation_lost:   0
+      reputation_lost:   0,
+      income_history:    [], # tracks 30 days of daily income
+      expense_history:   [], # tracks 30 days of daily spend
     }
   end
 
@@ -61,6 +63,14 @@ module Strategy
 
     # End of Week Eval
     def trigger_evaluation
+      # We know this is called at the end of a day,
+      # Add daily inc/exp to 30 days data
+      stats.income_history  << stats.money_gained
+      stats.expense_history << stats.money_spent
+      stats.income_history.shift  while stats.income_history.length  > 30
+      stats.expense_history.shift while stats.expense_history.length > 30
+      stats.money_gained = stats.money_spent = 0
+
       return unless stats.days_until_eval <= 0
       stats.days_until_eval = 7
       ScreenManager.open(WeeklyEvaluationScreen.new(stats))
@@ -78,17 +88,82 @@ module Strategy
   class WeeklyEvaluationScreen < Screen
     def initialize stats
       super()
+      # Sum the last 7 days to get the weekly spend/gain
+      weekly_spend = 0
+      weekly_gain  = 0
+      if stats.income_history.length > 6
+        weekly_spend = stats.expense_history[-7..-1].sum
+        weekly_gain  = stats.income_history[-7..-1].sum
+      end
       @model = {
         week:        stats.current_week,
-        money_gain:  stats.money_gained,
-        money_spend: stats.money_spent,
+        money_gain:  weekly_gain,
+        money_spend: weekly_spend,
         profit:      stats.money_gained - stats.money_spent,
       }
-      Strategy.eventbus.publish(Events::ENTER_SHOP)
+
+      # Build 2 graphs: income and expenses for the last 30 days
+      raise "Financial history data mismatch" unless stats.income_history.length == stats.expense_history.length
+      max_value = stats.income_history.max.greater(stats.expense_history.max)
+      @model.graph_data = { income: [], expense: [], scaled: false }
+      num_pts = stats.income_history.length
+      xscale  = 1.0 / (num_pts-1)
+      if num_pts > 1
+        (1..num_pts-1).each { |idx|
+          # Income graph
+          x1  = (idx-1) * xscale
+          x2  = idx     * xscale
+          pt1 = stats.income_history[idx-1]
+          pt2 = stats.income_history[idx]
+          @model.graph_data.income << {
+            x:  x1,
+            y:  pt1.fdiv(max_value),
+            x2: x2,
+            y2: pt2.fdiv(max_value),
+            g:  255
+          }
+
+          # Expense graph (same number of pts/x coords as income)
+          pt1 = stats.expense_history[idx-1]
+          pt2 = stats.expense_history[idx]
+          @model.graph_data.expense << {
+            x:  x1,
+            y:  pt1.fdiv(max_value),
+            x2: x2,
+            y2: pt2.fdiv(max_value),
+            r:  255
+          }
+        }
+      end
+
     end
 
-    def draw_func args, x, y, w, h, data = nil
+    def draw_graph args, x, y, w, h, data = nil
+      return if !data.expense or !data.income
+      # Offset & scale the data once, now that we know x/y/w/h
+      if !data.scaled
+        xscale = w - 3
+        yscale = h - 3
+        xoffs  = x + 1
+        yoffs  = y + 2
+        data.scaled = true
+        data.expense.each do |line|
+          line.x  = line.x  * xscale + xoffs
+          line.x2 = line.x2 * xscale + xoffs
+          line.y  = line.y  * yscale + yoffs
+          line.y2 = line.y2 * yscale + yoffs
+        end
+        data.income.each do |line|
+          line.x  = line.x  * xscale + xoffs
+          line.x2 = line.x2 * xscale + xoffs
+          line.y  = line.y  * yscale + yoffs
+          line.y2 = line.y2 * yscale + yoffs
+        end
+      end
+      args.outputs.lines << data.expense # red graph
+      args.outputs.lines << data.income  # green graph
       Rendering.set_color(COLOR_ORANGE_P8)
+      Rendering.text_left(x + 30, y + h, "Income/Expenses")
       Rendering.rectangle(x, y, w, h)
     end
 
@@ -100,7 +175,7 @@ module Strategy
       Gui.label("Cr spent: #{@model.money_spend}")
       Gui.label("Revenue: #{@model.profit}")
       Gui.label("You did good.")
-      Gui.draw_custom(200, 100, method(:draw_func))
+      Gui.draw_custom(200, 100, method(:draw_graph), @model.graph_data)
       Gui.label("Employees love you.")
       Gui.end_window()
     end
